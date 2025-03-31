425f657e60edf5d3c2a2d4626a6b2edd5ea21773
JCARNEIROX <j239738@dac.unicamp.br>
2025-03-31 14:06:27 -0300

    Update dos arquivos do primeiro projeto

---
+++ Projeto1/interface.py
@@ -0,0 +1,36 @@
+from machine import SoftI2C,Pin
+from ssd1306 import SSD1306_I2C
+
+
+# Configuração OLED
+i2c = SoftI2C(scl=Pin(15), sda=Pin(14))
+oled = SSD1306_I2C(128, 64, i2c)
+
+# Tela inicial
+def tela_inicial():
+    oled.fill(0)
+    oled.text("Reaction Time", 0, 0)
+    oled.text("Welcome to Reaction", 0, 12)
+    oled.text("Time Game.Define", 0, 21)
+    oled.text("players A and B.", 0, 31)
+    oled.show()
+
+# Função para exibir o tempo de reação no display OLED
+def display_oled(reacA,reacB):
+    oled.fill(0)  # Limpar display
+    oled.text("Reaction Time", 12, 0)
+    oled.text(" ", 0, 10)
+    oled.text("Last Times:", 0, 20)
+    #coloca um espaço em branco
+    oled.text(" ", 0, 30)
+    oled.text(f"Player A = {reacA}ms", 0, 40)
+    oled.text(f"Player B = {reacB}ms", 0, 50)
+    oled.show()
+
+# Função para exibir orientações no display OLED
+def start():
+    oled.fill(0)
+    oled.text("Press two bottons", 0, 0)
+    oled.text("to start and", 0, 10)
+    oled.text("get ready.", 0, 19)
+    oled.show()
--- Projeto1/main.py
+++ Projeto1/main.py
@@ -1,1 +1,117 @@
+from machine import Pin, PWM, SoftI2C
+from ssd1306 import SSD1306_I2C
+import interface
+import neopixel
+import time
+import random
 
+# Configuração OLED
+i2c = SoftI2C(scl=Pin(15), sda=Pin(14))
+oled = SSD1306_I2C(128, 64, i2c)
+
+# Inicializa a matriz de LEDs (5x5 = 25 LEDs)
+chain = neopixel.NeoPixel(Pin(7), 25)
+
+# Inicializa os botões
+button_a = Pin(5, Pin.IN, Pin.PULL_UP)
+button_b = Pin(6, Pin.IN, Pin.PULL_UP)
+
+# Define o pino para o buzzer
+buzzer = PWM(Pin(21))
+
+# Define as dimensões da matriz de LEDs
+matrix_width = 5
+matrix_height = 5
+
+# Define as cores
+COLOR_BLUE = (0, 0, 255)
+COLOR_RED = (10, 0, 0)
+COLOR_GREEN = (0, 10, 0)
+COLOR_OFF = (0, 0, 0)
+
+# Padrão da matriz inicial (todos os LEDs acesos)
+M_default = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1]
+]
+
+def draw_matriz(matriz, color):
+    """Exibe a matriz de LEDs."""
+    for i in range(25):
+        chain[i] = COLOR_OFF  # Apaga todos os LEDs
+    
+    for row in range(5):
+        for col in range(5):
+            if matriz[row][col] == 1:
+                index = row * 5 + col
+                chain[index] = color
+
+    chain.write()
+
+# Função para medir o tempo de reação
+def medir_reacao():
+    # Aguarda o verde ser aceso e o botão ser pressionado
+    start_time = time.ticks_ms()  # Inicia o contador de tempo
+    while True:
+        if (button_a.value() == 0) and (button_b.value() == 0):  # Botões pressionados novamente
+            reaction_time = time.ticks_ms() - start_time  # Calcula o tempo de reação
+            bip_buzzer(2000,0.3)  # Emite um bip de 0.3 segundos
+            return reaction_time
+
+# Função para fazer o bip no buzzer
+def bip_buzzer(duty,duration):
+    buzzer.freq(440)          # Define a frequência do som (440Hz, som padrão)
+    buzzer.duty_u16(duty)     # Define o volume do som (1000, volume médio)
+    time.sleep(duration)           # Duração do bip (0.1 segundos)
+    buzzer.duty_u16(0)        # Desliga o buzzer
+
+def contagem_regressiva():
+    time.sleep(1)
+    """Apaga os LEDs um por um antes de acender o verde."""
+    matriz = [row[:] for row in M_default]
+    for i in range(4, -1, -1):
+        for j in range(4, -1, -1):
+            matriz[i][j] = 0
+        draw_matriz(matriz, COLOR_RED)
+        bip_buzzer(700, 0.1)
+        time.sleep(1)  
+
+    time.sleep(random.uniform(1, 4))  # Aguarda um tempo aleatório antes de acender o verde
+    draw_matriz(M_default, COLOR_GREEN)  # Acende todos os LEDs verdes
+
+
+
+# Fica na tela de orientação enquanto não for definido os jogadores
+start_game = True
+reacA = 0
+reacB=  0    
+while True:
+    if start_game:
+        draw_matriz(M_default, COLOR_RED)  # Exibe os LEDs em vermelho inicialmente
+        interface.tela_inicial()  # Exibe o display OLED com os tempos de reação zerados
+    else:
+        draw_matriz(M_default, COLOR_RED)  # Exibe os LEDs em vermelho inicialmente
+        interface.display_oled(reacA,reacB)
+    if (button_a.value() == 0):
+        start_game = False
+        while not ((button_a.value() == 0) and (button_b.value() == 0)):
+                interface.start()
+        else:
+            contagem_regressiva()
+            reacA = medir_reacao()
+            
+            print(f"Tempo de reacao A: {reacA} ms")  # Exibe o tempo de reação
+    
+    if (button_b.value() == 0):
+        start_game = False
+        while not ((button_a.value() == 0) and (button_b.value() == 0)):
+                interface.start()
+        else:
+            contagem_regressiva()
+            reacB = medir_reacao()
+    
+            print(f"Tempo de reacao B: {reacB} ms")  # Exibe o tempo de reação
+            
